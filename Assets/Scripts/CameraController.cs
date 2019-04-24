438c0a299d9af5f4d267ec05826949b8d24f047c
Fabiola Dums <fdums@student.fitchburgstate.edu>
2019-04-23 20:16:29 -0400

    added ground, changed camera behavior

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,23 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject player;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offsetPosition;
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+    [SerializeField]
+    private bool lookAt = true;
 
-    private Vector3 offset;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        offset = transform.position - player.transform.position;
+        Refresh();
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    public void Refresh()
     {
-        transform.position = player.transform.position + offset;
-        //transform.rotation = player.transform.rotation;
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+            return;
+        }
+        // compute position
+        if (offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = target.position + offsetPosition;
+        }
+        // compute rotation
+        if (lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
     }
 }
